55c1f878adbf7a9d19639f8f944a8e4df54e2b32
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-07-06 18:09:54 -0300

    Extra

--- Training.py
+++ Training.py
@@ -2,4 +2,4 @@ N1 = int(input("Enter a number: "))
 N2 = int(input("Enter a another number: "))
 Result = N1 + N2
 
-print ("The addition of {} and {} is {}".format(N1, N2 ,Result))
+print ("The addition of {:=>10} and {:=<10} is {:=^10}".format(N1, N2 ,Result))
