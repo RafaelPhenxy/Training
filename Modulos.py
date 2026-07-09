cb350f7ba733a5f1c69494d5d02f1768d650d97f
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-07-09 18:32:09 -0300

    Import libr

---
+++ Modulos.py
@@ -0,0 +1,7 @@
+import math
+import random
+nm = int(input('Enter a number: '))
+RSq = math.sqrt(nm)
+print("Your number is: {} and your square root is: {:.2f}".format(nm, RSq))
+NRd = random.randint(1, 1000)
+print(NRd)
