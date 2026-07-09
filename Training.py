d5b76fea46cdc7ea043addfd1c9ce189b1992034
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-07-09 17:55:54 -0300

    .

--- Training.py
+++ Training.py
@@ -5,7 +5,7 @@ M = N1 * N2
 D = N1 / N2
 DW = N1 // N2
 P = N1 ** N2
-print ("The addition of {:=>10} and {:=<10} is {:=^10}".format(N1, "niger" ,Result))
+print ("The addition of {:=>10} and {:=<10} is {:=^10}".format(N1, N2 ,Result))
 print ("The addition is {}".format(N1+N2))
 print ("Multiplication is {}\n and division is {:.3f}".format(M, D), end= ', ')
 print ("division Whole is {} and potentiation is {}".format(DW, P))
